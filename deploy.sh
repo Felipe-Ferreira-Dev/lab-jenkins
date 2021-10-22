769e7895e9f6ef16532acd190ce4675bc7ef3f7f
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-22 17:52:59 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ cd terraform
 echo "Aguardando criação de maquinas ..."
 sleep 15 # 15 segundos
 
-echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
-echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+sudo echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
+sudo echo "$(/home/ubuntu/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
 
 echo "Aguardando criação de maquinas ..."
 sleep 15 # 15 segundos

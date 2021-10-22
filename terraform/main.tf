47298cfdb1dfd2fac6c775d89e799b5efd6dd406
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-22 12:19:45 -0300

    add output e ajustes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2_nginx" {
+resource "aws_instance" "ec2_jenkins_nginx" {
   ami                         = "ami-09e67e426f25ce0d7"
   instance_type               = "t2.micro"
   key_name                    = "key-dev-tf"
@@ -10,13 +10,13 @@ resource "aws_instance" "ec2_nginx" {
   }
    
   tags = {
-    Name = "ec2-nginx-ffaihdw"
+    Name = "ec2-jenkins-nginx-ffaihdw"
   }
-  vpc_security_group_ids = ["${aws_security_group.sg_nginx.id}"]
+  vpc_security_group_ids = ["${aws_security_group.sg_jenkins_nginx.id}"]
 }
 
-resource "aws_security_group" "sg_nginx" {
-  name        = "sg_nginx_ffaihdw"
+resource "aws_security_group" "sg_jenkins_nginx" {
+  name        = "sg_jenkins_nginx_ffaihdw"
   description = "sg nginx and ssh inbound traffic"
   vpc_id      = "vpc-080da39cf7b8a7fdc"
 
@@ -60,8 +60,20 @@ resource "aws_security_group" "sg_nginx" {
   ]
 
   tags = {
-    Name = "sg_nginx_ffaihdw"
+    Name = "sg_jenkins_nginxx_ffaihdw"
   }
+}
+
+# terraform refresh para mostrar o ssh
+output "output_jenkins_nginx" {
+  value = [
+    "ec2_jenkins_nginx",
+    "id: ${aws_instance.ec2_jenkins_nginx.id}",
+    "private: ${aws_instance.ec2_jenkins_nginx.private_ip}",
+    "public: ${aws_instance.ec2_jenkins_nginx.public_ip}",
+    "public_dns: ${aws_instance.ec2_jenkins_nginx.public_dns}",
+    "ssh -i ssh -i ~/.ssh/id_rsa_itau ubuntu@${aws_instance.ec2_jenkins_nginx.public_dns}"
+  ]
 }
 
 

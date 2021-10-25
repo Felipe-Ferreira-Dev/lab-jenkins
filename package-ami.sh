d875c0f2b94ed14adba6457e7067374237f24bb5
Felipe Ferreira <62390835+Felipe-Ferreira-Dev@users.noreply.github.com>
2021-10-25 10:09:22 -0300

    Update package-ami.sh

--- package-ami.sh
+++ package-ami.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
@@ -9,15 +8,4 @@ RESOURCE_ID=$(/home/ubuntu/terraform output | grep resource_id | awk '{print $2;
 cd ../terraform-ami
 /home/ubuntu/terraform init
 TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID /home/ubuntu/terraform apply -auto-approve
-=======
-#!/bin/bash
 
-VERSAO=$(git describe --tags $(git rev-list --tags --max-count=1))
-
-cd /terraform
-RESOURCE_ID=$(/home/ubuntu/terraform output | grep resource_id | awk '{print $2;exit}' | sed -e "s/\",//g")
-
-cd terraform-ami
-/home/ubuntu/terraform init
-TF_VAR_versao=$VERSAO TF_VAR_resource_id=$RESOURCE_ID /home/ubuntu/terraform apply -auto-approve
->>>>>>> 981c7aa4b4e3b83cc1083b080c98291dd4c070f4
